71b1d0b4a4fc050ea813d2f75ad59b5af2ccc4b1
Sten Hougaard <netsi1964@gmail.com>
2022-05-01 15:40:00 +0200

    Added distance function

--- svg.js
+++ svg.js
@@ -5,6 +5,13 @@ export function screen2svg (eleSvg, x, y) {
   return point.matrixTransform(eleSvg.getScreenCTM().inverse())
 }
 
+export function distance (point1, point2) {
+  return Math.sqrt(
+    Math.pow(point2.x - point1.x, 2) + Math.pow(point2.y - point1.y, 2)
+  )
+}
+
 export default {
-  screen2svg
+  screen2svg,
+  distance
 }
